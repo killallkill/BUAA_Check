6ed336f9ffaffbf0213e6a78847d6a5de6533f8e
XingJingxuan <xjx524@gmail.com>
2017-01-04 14:04:18 +0800

    检查列表页相关实体类

---
+++ app/src/main/java/edu/buaa/bwc/buaa_check/POJOs/CheckCheckItem.java
@@ -0,0 +1,25 @@
+package edu.buaa.bwc.buaa_check.POJOs;
+
+/**
+ * Created by XJX on 2017/1/3.
+ */
+
+public class CheckCheckItem {
+
+    public String content;
+    public String id;
+    public String rectifyState;
+    public String name;
+    public String userId;
+    public String path;
+    public String checkAdd;
+    public String checkTime;
+    public String passRate;
+    public String rectifyStateVal;
+    public String qualified;
+
+    @Override
+    public String toString() {
+        return id + " " + name + " " + checkTime;
+    }
+}
